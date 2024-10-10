eb72d67c58e94cf2690cee05fa2cfc4e879667f0
Aadil Nazir <aadiljanjooh786@gmail.com>
2024-10-10 23:05:39 +0530

    Resolved conflicts

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "8c99ffdfe6c14b56c8b760cbadd4764b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
